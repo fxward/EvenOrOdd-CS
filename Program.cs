5fe47f3a1f4536e362e89ebcfc6a53e79a3d457d
FXWARD <169081624+fxward@users.noreply.github.com>
2024-08-19 12:20:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// github.com/fxward cs
+// EvenOrOdd
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Made by github.com/fxward");
+        Console.Write("Type a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("\nNumber is Even");
+        }
+        else 
+        {
+            Console.WriteLine("Number is Odd");
+        }
+    }
+}
